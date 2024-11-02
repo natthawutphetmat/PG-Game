e88f491ba05f46da95917031da2c36b7c555fce3
exadsdev <exadsdev@gmail.com>
2024-11-02 20:21:53 +0700

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,11 +10,14 @@ export default function Page() {
   };
 
   return (
-    <header className="header">
+    <header className="header">  
       <div className="main">
         <div className="box">
           <button className='btnhome' onClick={Signup}>
             <div className="item">
+
+            <h3>PGGameเล่นกันแบบสนุกๆ|เล่นกันได้ตลอดเวลา24ชม </h3>
+
               <img src="/img/PG01.webp" className="itemimg" alt="Image 1" />
               <img src="/img/btn01.gif" className="itemimg" alt="Button 1" />
               <img src="/img/PG02.webp" className="itemimg" alt="Image 2" />
@@ -23,7 +26,29 @@ export default function Page() {
               <img src="/img/btn02.gif" className="itemimg" alt="Button 2" />
               <img src="/img/PG04.webp" className="itemimg" alt="Image 4" />
               <img src="/img/btn02.gif" className="itemimg" alt="Button 2" />
-              {/* Add more images as needed */}
+              <h3>
+              เล่นPGGameออนไลน์ฟรี|เกมPGเล่นได้ทุกที่ทุกเวลา <br />
+              เล่นPGGameออนไลน์ฟรี|เกมPGเล่นได้ทุกที่ทุกเวลา<br />
+              ท้าประลองกับบอทในเกมPG|เกมหมากกระดานPageGame <br />
+              เกมง่ายๆสนุกและคลาสสิก|เกมกลยุทธ์PageGameฟรี<br />
+              เล่นPGกับเพื่อนออนไลน์|เกมPGรองรับทุกขนาดจอ<br />
+              เกมPG-Gameสำหรับเด็ก|เล่นเกมPGไม่ต้องติดตั้ง<br />
+              เกมPGกับบอทอัจฉริยะ|เกมPGบนเว็บมือถือฟรี<br />
+              สนุกกับเกมPGในเบราว์เซอร์|เกมPGออนไลน์เล่นง่าย<br />
+              PG-Gameกลยุทธ์และทักษะ|เล่นเกมPGไร้ขีดจำกัด<br />
+              เล่นPG-Gameไม่ต้องโหลด|เกมPGออนไลน์ฟรีทุกอุปกรณ์<br />
+              เกมPGใช้สมองและความคิด|PG-Gameไม่ต้องดาวน์โหลด<br />
+              เกมกระดานคลาสสิกฟรี|เล่นPGด้วยมือถือหรือแท็บเล็ต<br />
+              เกมPGออนไลน์สนุกเพลิน|เกมกลยุทธ์PGสุดฮิต<br />
+              เล่นPG-Gameได้ทันที|เกมหมากกระดานPGบนเว็บ<br />
+              ตอบโจทย์ผู้เล่นทุกวัย|เกมPGไม่มีโฆษณากวนใจ<br />
+              เกมPGที่เด็กและผู้ใหญ่ชอบ|เกมPGเสริมทักษะและสมาธิ<br />
+
+
+
+              </h3>
+           
+           
             </div>
           </button>
         </div>
